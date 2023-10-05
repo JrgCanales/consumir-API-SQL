fd243112c5b5fda2eb762fcda58d0647c089bb56
JrgCanales <jorgecanalesortega935@gmail.com>
2023-10-05 13:26:28 -0600

    sp client modified

--- stored_procedures/AllProcedures.sql
+++ stored_procedures/AllProcedures.sql
@@ -109,17 +109,28 @@ GO
 CREATE OR ALTER PROCEDURE sp_HttpClient @url VARCHAR(MAX), @result VARCHAR(MAX) OUTPUT
 AS
 BEGIN
-    DECLARE @token INT;
-    DECLARE @ret INT;
-    DECLARE @json AS TABLE(Json_Table NVARCHAR(MAX))
+    DECLARE @intToken INT,
+			@object INT,
+			@statusText NVARCHAR(32);
 
-    EXEC @ret = sp_OACreate 'MSXML2.XMLHTTP', @token OUT;
-    EXEC @ret = sp_OAMethod @token, 'open', NULL, 'GET' , @url, 'false';
-    EXEC @ret = sp_OAMethod @token, 'send';
+    DECLARE @json AS TABLE(Json_Table NVARCHAR(MAX));
 
-    INSERT INTO @json (Json_Table) EXEC sp_OAGetProperty @token, 'responseText'
+    EXEC @object = sp_OACreate 'MSXML2.XMLHTTP', @intToken OUT;
+    EXEC @object = sp_OAMethod @intToken, 'open', NULL, 'GET' , @url, 'false';
+    EXEC @object = sp_OAMethod @intToken, 'send';
+	EXEC @object = sp_OAGetProperty @intToken, 'statusText', @statusText OUT;
 
-    SELECT @result = Json_Table FROM @json;
+    INSERT INTO @json (Json_Table) EXEC sp_OAGetProperty @intToken, 'responseText';
+
+	IF (@statusText <> 'OK')
+	BEGIN
+		DECLARE @ErroMsg NVARCHAR(40) = 'An error occurred in the request.';
+		PRINT @ErroMsg;
+
+		RETURN;
+	END
+
+	SELECT @result = Json_Table FROM @json;
 END
 GO
 
